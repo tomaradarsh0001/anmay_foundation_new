7f4f217f619164855b8d808f8b356f51ff996052
Adarsh Tomar <tomaradarsh0001@gmail.com>
2026-02-11 16:07:58 +0530

    finalised website

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid bg-dark text-white-50 footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
     <div class="container py-5">
         <div class="row g-5">
-            <div class="col-lg-6 col-md-6">
+            <div class="col-lg-5 col-md-6">
                 <h1 class="fw-bold text-primary mb-4">Anmay<span class="text-white">Foundation</span></h1>
                 <p>Anmay Foundation stands for dignity, inclusion, and hope for every disabled child.Together, we nurture abilities and build a kinder, stronger society.</p>
                 <div class="d-flex pt-2">
@@ -11,13 +11,13 @@
                     <a class="btn btn-square me-0" href="{{ $websiteDetails->linkedin ?? 'Not set' }}"><i class="fab fa-linkedin-in"></i></a>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6">
+            <div class="col-lg-5 col-md-6">
                 <h5 class="text-light mb-4">Address</h5>
                 <p><i class="fa fa-map-marker-alt me-3"></i>{{ $websiteDetails->address ?? 'Not set' }}</p>
                 <p><i class="fa fa-phone-alt me-3"></i>{{ $websiteDetails->phone ?? 'Not set' }}</p>
                 <p><i class="fa fa-envelope me-3"></i>{{ $websiteDetails->email ?? 'Not set' }}</p>
             </div>
-            <div class="col-lg-3 col-md-6">
+            <div class="col-lg-2 col-md-6">
                 <h5 class="text-light mb-4">Quick Links</h5>
                 <a class="btn btn-link" href="/">Home</a>
                 <a class="btn btn-link" href="{{ route('about') }}">About Us</a>
@@ -32,7 +32,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                    &copy; <a href="#">anmayfoundation.org</a>, All Right Reserved.
+                    &copy; <a href="mailto:anmayfoundation.org">anmayfoundation.org</a>, All Right Reserved.
                 </div>
             </div>
         </div>
